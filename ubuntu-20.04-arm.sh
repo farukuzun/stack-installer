8b51f66bea5c3d9986a6b159e9d47ca901917777
Faruk Uzun <mail@github.farukuzun.com>
2021-05-19 15:22:49 +0300

    arm64 update

--- ubuntu-20.04-arm.sh
+++ ubuntu-20.04-arm.sh
@@ -12,7 +12,9 @@ echo "installing Python 3.x extensions ..."
 sudo apt-get -y install gcc binutils
 sudo apt-get -y install software-properties-common
 sudo apt-get -y install python3-dev python3-pip python3-setuptools
+sudo apt-get -y install python-is-python3
 sudo apt-get -y install build-essential
+sudo apt-get -y install llvm llvm-dev
 sudo -H pip3 install --upgrade pip
 hash -d pip3
 pip3 install --upgrade setuptools
@@ -25,6 +27,8 @@ sudo apt-get -y install libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libre
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz -q
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
+wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -O config.guess
+wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' -O config.sub
 ./configure --prefix=/usr
 make
 sudo make install
@@ -35,6 +39,7 @@ cd
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update -y
 sudo apt-get install -y postgresql postgresql-contrib python3-psycopg2 libpq-dev
+sudo systemctl restart postgres
 
 # install screen
 echo "installing Screen ..."
@@ -45,8 +50,7 @@ echo "installing jesse ..."
 pip3 install -r https://raw.githubusercontent.com/jesse-ai/jesse/master/requirements.txt
 pip3 install jesse
 
-# point python to python3
-echo "alias python=python3" >> ~/.profile
+# point pip to pip3
 echo "alias pip=pip3" >> ~/.profile
 . ~/.profile
 
@@ -61,7 +65,11 @@ python --version
 echo "Here's the output of 'pip --version':"
 pip --version
 
-# install Oh My Zsh
-echo "installing Oh My Zsh"
-sudo apt-get install -y git zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir quant
+
+echo "Environment Setup"
+sudo su - postgres
+psql
+CREATE DATABASE jesse_db2;
+CREATE USER jesse_user WITH PASSWORD 'password';
+GRANT ALL PRIVILEGES ON DATABASE jesse_db to jesse_user;
